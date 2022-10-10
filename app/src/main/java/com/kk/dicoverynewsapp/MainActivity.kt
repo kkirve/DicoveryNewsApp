a0621cc1dc75d6c46f3005e229b42f6c6e7d4953
KK68855 <k.kirve@zensar.com>
2022-10-10 19:18:54 +0530

    UI

--- app/src/main/java/com/kk/dicoverynewsapp/MainActivity.kt
+++ app/src/main/java/com/kk/dicoverynewsapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.kk.dicoverynewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kk.dicoverynewsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+
+
+
+    }
+    fun buttonCanadaClick()
+    {
+        binding.textViewSelectedCountry.text="Canada Selected"
+    }
+    fun buttonUSClick()
+    {
+        binding.textViewSelectedCountry.text="US Selected"
+    }
+    fun buttonIndiaClick()
+    {
+        binding.textViewSelectedCountry.text="India Selected"
     }
 }
