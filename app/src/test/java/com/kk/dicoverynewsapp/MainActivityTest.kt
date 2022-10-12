594cee509bb8bf152edfbb62af6e7c99edb8cc4d
KK68855 <k.kirve@zensar.com>
2022-10-12 12:51:04 +0530

    add testcase for searcg query less than 2 letter and sort by should be from predecied list only

--- app/src/test/java/com/kk/dicoverynewsapp/MainActivityTest.kt
+++ app/src/test/java/com/kk/dicoverynewsapp/MainActivityTest.kt
@@ -67,4 +67,32 @@ class MainActivityTest{
         )
         assertThat(result).isTrue()
     }
+
+
+    @Test
+    fun false_Value_for_sortby_return_false()
+    {
+        //sortby should from internal list popularity,language
+        val result = WebserviceUtil.isValidWebCall(
+            "Canada",
+            "2022-10-10",
+            "popular",
+            "22bb22af9cdd4fc5ab21d3eefc387b09"
+        )
+        assertThat(result).isFalse()
+    }
+
+
+    @Test
+    fun less_2_letter_search_query_return_false()
+    {
+        val result = WebserviceUtil.isValidWebCall(
+            "C",
+            "2022-10-10",
+            "popularity",
+            "22bb22af9cdd4fc5ab21d3eefc387b09"
+        )
+        assertThat(result).isFalse()
+    }
+
 }
